1336bb4ab0902311a1674944fe5a0fcba4087870
gorold <woogerald@yahoo.com.sg>
2020-03-12 00:02:30 +0800

    Added graph_reader.go, structs and methods to read and encode json message structure

---
+++ src/pregol/graph_reader.go
@@ -0,0 +1,54 @@
+package pregol
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type infoReader struct {
+	Name          string
+	NumVertices   int
+	NumPartitions int
+}
+
+type vertexReader struct {
+	Name  string
+	Value float64
+}
+
+type edgeReader struct {
+	VerticeID int
+	Value     float64
+}
+
+type graphReader struct {
+	Info        infoReader
+	Vertices    map[int]vertexReader
+	Edges       map[int][]edgeReader
+	ActiveNodes []ActiveNode
+}
+
+func getGraphFromFile(graphFile string) *graphReader {
+	jsonFile, err := os.Open(graphFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteVal, _ := ioutil.ReadAll(jsonFile)
+	var g graphReader
+	json.Unmarshal(byteVal, &g)
+	fmt.Println(g)
+	return &g
+}
+
+func getGraphFromJSONByte(jsonBytes []byte) *graphReader {
+	var g graphReader
+	json.Unmarshal(jsonBytes, &g)
+	fmt.Println(g)
+	return &g
+}
