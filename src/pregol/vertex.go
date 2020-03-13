df992bd587201e1a7bac755b2e18f9af8b57ffda
luoqichan <40707452+luoqichan@users.noreply.github.com>
2020-03-13 16:10:45 +0800

    vertex skeleton small update

--- src/pregol/vertex.go
+++ src/pregol/vertex.go
@@ -1,10 +1,5 @@
 package pregol
 
-import (
-	"fmt"
-	"strconv"
-)
-
 type Message struct {
 	recID int
 	val   int
@@ -14,38 +9,32 @@ type Vertex struct {
 	id       int
 	flag     bool
 	val      float64
-	inEdges  chan map[string]float64
-	outEdges map[string]float64
+	nowEdge  map[int]float64
+	inEdges  chan map[int]float64
+	outEdges map[int]float64
 	vertices []Vertex // do i know my peers?
 }
 
-func (v *Vertex) compute(UDF func(placeholder interface{}) interface{}, owner Worker) {
+type UDF func(vertex *Vertex) (bool, map[int]float64)
+
+func (v *Vertex) compute(udf UDF, owner Worker) {
 	// do computations by iterating over messages from each incoming edge.
 	select {
-	case msg := <-v.inEdges:
-		v.flag = true
-		for i := range msg {
-			//TODO: Get return values from UDF
+	case v.nowEdge = <-v.inEdges:
+		v.flag, v.outEdges = udf(v)
 
-		}
-
-		//TODO: Map return values {outEdge (string): value float64 ...}
-		//TODO: set outgoing edges v.outEdges = xxx
 		//TODO: worker-side need channel to receive incoming messages for this super step : inChan []chan map[string]float64
 		owner.inChan[v.id] <- v.outEdges
 
 	default:
-		v.flag = false
-		v.vote_to_halt()
+		if v.flag {
+			v.VoteToHalt(owner)
+		}
 	}
 }
 
 // TODO: Worker-side need a channel to receive votes to halt from each worker
 // halt []chan
-func (v *Vertex) vote_to_halt() {
-	v.worker.halt[v.id] <- 0
-}
-
-func main() {
-	fmt.Println("hello world")
+func (v *Vertex) VoteToHalt(owner Worker) {
+	owner.halt[v.id] <- 0
 }
